a72dea32aa231da823f0cc8f0a18f1f6716a51a7
mintyry <gitmintyry@gmail.com>
2023-05-31 22:48:57 -0700

    added JS

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var wrestlers = ['Shawn Michaels', 'Bret Hart', 'Stone Cold', 'The Rock'];
+
+for (var x = 0; x < wrestlers.length; x++) {
+ console.log(wrestlers[x]);
+}
+
+/*
+if (topic === 'Shawn Michaels') {
+ console.log("Shawn Michaels is the GOAT!");
+} else if (topic === 'Bret Hart') {
+ console.log("Bret Hart is the GOAT!");
+} else if (topic === 'Stone Cold') {
+ console.log("Stone Cold is the GOAT!");
+} else if (topic === 'The Rock') {
+ console.log("The Rock is the GOAT!");
+} else {
+ console.log('Tell me you did not just say that!');
+}
+*/
