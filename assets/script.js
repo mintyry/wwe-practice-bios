354a6b47acc0a7e1a3b2e4e19508d727207f18d5
mintyry <gitmintyry@gmail.com>
2023-06-02 19:21:13 -0700

    added JS functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 var wrestlers = ['Shawn Michaels', 'Bret Hart', 'Stone Cold', 'The Rock'];
+var randomWrestler = wrestlers[Math.floor(Math.random() * wrestlers.length)];
 
-for (var x = 0; x < wrestlers.length; x++) {
- console.log(wrestlers[x]);
+function listWrestlers() {
+    for (var x = 0; x < wrestlers.length; x++) {
+    console.log(wrestlers[x]);
+    }
 }
 
-/*
-if (topic === 'Shawn Michaels') {
- console.log("Shawn Michaels is the GOAT!");
-} else if (topic === 'Bret Hart') {
- console.log("Bret Hart is the GOAT!");
-} else if (topic === 'Stone Cold') {
- console.log("Stone Cold is the GOAT!");
-} else if (topic === 'The Rock') {
- console.log("The Rock is the GOAT!");
-} else {
- console.log('Tell me you did not just say that!');
+function selectWrestler() {
+    if (randomWrestler === 'Shawn Michaels') {
+    console.log("Shawn Michaels is the GOAT!");
+    } else if (randomWrestler === 'Bret Hart') {
+    console.log("Bret Hart is the best there is, was, and ever will be!");
+    } else if (randomWrestler === 'Stone Cold') {
+    console.log("Stone Cold is the GOAT, WHAT?");
+    } else if (randomWrestler === 'The Rock') {
+    console.log("The Rock is the GOAT!");
+    } else {
+    console.log('Tell me you did not just say that!');
+    }
 }
-*/
+
+console.log('Here are the wrestlers we have presented:');
+listWrestlers();
+console.log('Which wrestler is the greatest of all time?');
+selectWrestler();
